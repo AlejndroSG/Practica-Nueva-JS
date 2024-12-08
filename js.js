c562eca5dfd72a54a7deda57e3da102658a454e8
AlejndroSG <alex2005sg@gmail.com>
2024-12-08 15:26:50 +0100

    Finalizamos APPI de Recetas en JS

--- js.js
+++ js.js
@@ -1,50 +1,26 @@
+// VARIABLES COMPARTIDAS EN LOS DOS HTML
 
 let result = window.location.href.split("/");
 let resultF = result[result.length-1];
-const recetasTotales = [];
-console.log(resultF);
+let recetasTotales = [];
+let recetaFav = null;
+if(localStorage.getItem("recetaFav")){
+    recetasTotales = JSON.parse(localStorage.getItem("recetaFav"));
+}
+const toastLiveExample = document.getElementById('liveToast')
+const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastTrigger2 = document.getElementById('liveToastBtn2')
 
-if(resultF == "index.html"){
-    let recetaFav = null;
-    //VARIABLES NECESARIAS Y CONSTANTES
+
+if(resultF == "index.html"){ //Compruebo si me encuentro en index o en favs
+    //VARIABLES NECESARIAS PARA EL INDEX
+
     let selectCategorias = document.querySelector("#categorias select");
     let divComidas = document.querySelector("#recetas .row");
-    let divs = null;
-    const toastTrigger = document.getElementById('liveToastBtn')
-    const toastTrigger2 = document.getElementById('liveToastBtn2')
     
-    const toastLiveExample = document.getElementById('liveToast')
-
-    
-    
-    // FUNCIONALIDADES
-    
+    // FUNCIONALIDADES DEL INDEX
     // Funcion para el fetching de datos de la API con la URL
-        const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
-        toastTrigger.addEventListener('click', () => {
-            toastLiveExample.innerHTML = `
-                <div class="toast-header">
-                    <strong class="me-auto">R<span>A</span>PPI</strong>
-                    <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-                    </div>
-                    <div class="toast-body">Añadido a Favoritos con éxito</div>
-                </div>
-            `;
-            toastBootstrap.show()
-            })
-        // const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
-        toastTrigger2.addEventListener('click', () => {
-            toastLiveExample.innerHTML = `
-                <div class="toast-header">
-                    <strong class="me-auto">R<span>A</span>PPI</strong>
-                    <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-                    </div>
-                    <div class="toast-body">Eliminado de Favoritos con éxito</div>
-                </div>
-            `;
-            toastBootstrap.show()
-        })
-
     function cargarCategorias(){
         const url = "https://www.themealdb.com/api/json/v1/1/categories.php"
         obtenerDatos(url).then(datos =>{
@@ -59,19 +35,16 @@ if(resultF == "index.html"){
     function mostrarRecetasCatSeleccionada(categoria){
         limpiar(divComidas);
         const url2 = "https://www.themealdb.com/api/json/v1/1/filter.php?c="+categoria;
-        // console.log(url2);
         obtenerDatos(url2)
         .then(datos =>{
-            // console.log(datos.meals);
             datos.meals.forEach(dato => {
-                // console.log(dato);
                 divComidas.innerHTML += `
                 <div class="col-md-4">
                     <div class="card text-white bg-primary mb-3">
                         <div class="card-header"><img class="img-fluid" src="${dato.strMealThumb}" alt=""></div>
                         <div class="card-body">
                             <h4 class="card-title">${dato.strMeal}</h4>
-                            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#emergente" onclick=mostrarReceta(${dato.idMeal}) >Ver la receta</button>
+                            <button type="button" class="btn btn-danger col-12" data-bs-toggle="modal" data-bs-target="#emergente" onclick=mostrarReceta(${dato.idMeal}) >Ver la receta</button>
                         </div>
                     </div>
                 </div>
@@ -80,12 +53,9 @@ if(resultF == "index.html"){
         });
     }
 
-    
-    const todosLosModales = document.querySelectorAll(".modal");
-
     cargarCategorias();
 }else{
-    // const recetasTotales = JSON.parse(localStorage.getItem("recetaFav"));
+    // FUNCIONALIDADES DE FAVORITOS
     const contenedorRecetas = document.querySelector("#recetas .row");
     if(JSON.parse(localStorage.getItem("recetaFav")) == "" || !JSON.parse(localStorage.getItem("recetaFav"))){
         let mensaje = document.createElement("h2");
@@ -93,7 +63,6 @@ if(resultF == "index.html"){
         mensaje.textContent = "Aun no tienes ninguna receta en Favoritos";
         document.querySelector(".row > h1").append(mensaje);
     }else{
-        const recetasTotales = JSON.parse(localStorage.getItem("recetaFav"));
         recetasTotales.forEach(receta => {
             contenedorRecetas.innerHTML += `
             <div class="col-md-4">
@@ -110,6 +79,7 @@ if(resultF == "index.html"){
     }
 }
 
+// TODAS LAS FUNCIONES UTILES
 function obtenerDatos(url){
     return fetch(url).then(respuesta => respuesta.json());
 }
@@ -119,16 +89,12 @@ function limpiar(cosa){
 }
 
 function mostrarReceta(idReceta){
-    console.log(idReceta);
     let ul = document.querySelector(".modal ul");
-    console.log(ul);
     if(ul != null){
         limpiar(ul);
     }
     const url3 = "https://www.themealdb.com/api/json/v1/1/lookup.php?i="+idReceta;
     obtenerDatos(url3).then(receta =>{
-        console.log(receta);
-        console.log(receta.meals[0].strMeal);
         let titulo = document.querySelector(".modal-title");
         titulo.innerHTML = `
             <h1 class="modal-title fs-5" id="emergenteLabel">${receta.meals[0].strMeal}</h1>
@@ -137,7 +103,7 @@ function mostrarReceta(idReceta){
         img.setAttribute("src",receta.meals[0].strMealThumb);
         let p = document.querySelector(".modal-body p");
         p.innerHTML = `
-            <p>${receta.meals[0].strInstructions}</p>
+            <p class="my-3">${receta.meals[0].strInstructions}</p>
         `
         
         for (let i = 1; i <= 20; i++) {
@@ -149,18 +115,11 @@ function mostrarReceta(idReceta){
         }
 
         recetaFav = {
-            id: receta.meals[0].idMeal,
+            id: idReceta,
             nombreReceta: receta.meals[0].strMeal,
             img: receta.meals[0].strMealThumb,
             instrucciones: receta.meals[0].strInstructions
         }
-
-        // let hola = titulo.closest(".modal").querySelector(".modal-footer button:nth-child(1)");
-        // console.log(hola);
-
-        // console.log(titulo.parentNode.querySelector(".modal-footer button:nth-child(1)"));
-
-        // let butAñadirFavs = titulo.closest(".modal").querySelector(".modal-footer button:nth-child(1)");
 
         let but1 = document.querySelector(".modal-footer button:nth-child(1)");
         let but2 = document.querySelector(".modal-footer button:nth-child(2)");
@@ -181,13 +140,11 @@ function mostrarReceta(idReceta){
 
 function modificarBotones(butAñadirFavs, butEliminarFavs){
     butAñadirFavs.addEventListener("click", () =>{
-        // butAñadirFavs.textContent = "Eliminar de Favoritos";
         subirLocalStorage(recetaFav);
         butAñadirFavs.style.display = "none";
         butEliminarFavs.style.display = "block";
     });
     butEliminarFavs.addEventListener("click", () =>{
-        // butAñadirFavs.textContent = "Añadir a Favoritos";
         eliminarLocalStorage(recetaFav);
         butAñadirFavs.style.display = "block";
         butEliminarFavs.style.display = "none";
@@ -215,5 +172,29 @@ function eliminarLocalStorage(recetasFav){
         }
     });
 }
+
+toastTrigger.addEventListener('click', () => {
+    toastLiveExample.innerHTML = `
+        <div class="toast-header">
+            <strong class="me-auto">R<span>A</span>PPI</strong>
+            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div class="toast-body">Añadido a Favoritos con éxito</div>
+        </div>
+    `;
+    toastBootstrap.show()
+    })
+// const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+toastTrigger2.addEventListener('click', () => {
+    toastLiveExample.innerHTML = `
+        <div class="toast-header">
+            <strong class="me-auto">R<span>A</span>PPI</strong>
+            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div class="toast-body">Eliminado de Favoritos con éxito</div>
+        </div>
+    `;
+    toastBootstrap.show()
+})
 
 
